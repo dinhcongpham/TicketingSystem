f7e1f0b34c2ca1ec065017b31efb0d96688bdf8b
cong <phamdinhcong0@gmail.com>
2025-06-24 22:14:19 +0700

    refactor(frontend): implement api endpoint

---
+++ Front-end/Services/ApiUrlBuilder.cs
@@ -0,0 +1,23 @@
+﻿namespace Front_end.Services
+{
+    public interface IApiUrlBuilder
+    {
+        string Build(string relativePath);
+    }
+
+    public class ApiUrlBuilder : IApiUrlBuilder
+    {
+        private readonly string _baseUrl;
+
+        public ApiUrlBuilder(IConfiguration config)
+        {
+            _baseUrl = config["ApiGateway:BaseUrl"];
+        }
+
+        public string Build(string relativePath)
+        {
+            return $"{_baseUrl}{relativePath}";
+        }
+    }
+
+}
